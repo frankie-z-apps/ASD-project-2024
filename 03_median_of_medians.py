9bbce5159944f8789504bdb7eec73f985a82c471
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-05-13 23:44:55 +0200

    polish code and add comments for procedure

--- 03_median_of_medians.py
+++ 03_median_of_medians.py
@@ -1,5 +1,3 @@
-# LAB ES3
-
 from math import ceil
 
 def input_array():
@@ -21,7 +19,6 @@ def partition(a, low, high):
     i = low
     for j in range(low, high-1):
         if a[j] <= p:
-            #scambio a[i] con a[j]
             a[i], a[j] = a[j], a[i]
             i += 1
     a[i], a[high-1] = a[high-1], a[i]
@@ -34,61 +31,42 @@ def median_of_medians(arr, low, high, k):
         insertion_sort(arr, low, high)
         return arr[k-1]
     else:
-        z = low #NB
-        counter_medians = 0
-
+        z = low
+        medians_counter = 0
+        # 1 - divisione dell'array in blocchi di 5 elementi
         for i in range(low, high, 5):
-
-            if i + 5 > high: #NB
+            # 2 - ordinamento e calcolo della mediana di ciascun blocco
+            if i + 5 > high:
                 insertion_sort(arr, i, high)
-                # print(f"mediano dell'ultimo blocco: {arr[(len(arr) + i)//2]}")
-                arr[z], arr[ ceil((high + i)/2)-1 ] = arr[ ceil((high + i)/2)-1 ], arr[z] #NB
+                arr[z], arr[ ceil((high + i)/2)-1 ] = arr[ ceil((high + i)/2)-1 ], arr[z]
             else:
                 insertion_sort(arr, i, i+5)
                 arr[z], arr[i+2] = arr[i+2], arr[z]
 
             z += 1
-            counter_medians += 1
+            medians_counter += 1
             
-        
-    # versione ricorsiva:
-    if (counter_medians % 2 == 0): #NB
-        m = median_of_medians(arr, low, z, (counter_medians//2)+low)
+    # 3 - calcolo della mediana delle mediane tramite chiamata ricorsiva
+    if (medians_counter % 2 == 0):
+        m = median_of_medians(arr, low, z, (medians_counter//2)+low)
     else:
-        m = median_of_medians(arr, low, z, ((counter_medians//2)+1)+low)
+        m = median_of_medians(arr, low, z, ((medians_counter//2)+1)+low)
     ind = arr.index(m)
     
-    arr[ind], arr[high-1] = arr[high-1], arr[ind] # scambio m con l'ultimo elemento dell'array 
+    arr[ind], arr[high-1] = arr[high-1], arr[ind]
 
     # 4 - partizionamento dell'array originario con perno la mediana delle mediane
-    
     r = partition(arr, low, high)
-    #print(f"median of medians: {m}")
-    #print(f"array: {arr}")
-    #print(f"partition(array): {arr[r]}")
 
     # 5 - chiamata ricorsiva a dx o sx a seconda del valore di k
     if r == k-1:
         return arr[r]
-    elif r > k-1: # altrimenti cerco a sinistra o destra, in base alla grandezza di k
+    elif r > k-1:
         return median_of_medians(arr, low, r, k)
     else: 
         return median_of_medians(arr, r+1, high, k)
     
 
-#a = input_array()
-#k = int(input())
-#print(median_of_medians(a, 0, len(a), k))
-
-
-#PROVE
-
-myarr = [25, 12, 8, 9, 3, 7, 52, 10, -2, 24, 5, 29, 59, 23]
-myarr2 = [12, 32, 5, 6, 3, 69]
-myarr3 = [9, -2, 7, 8, 5, 3, 10, 12, 23, 29, 52, 59, 24, 25]
-#myarr-sorted = [-2, 3, 5, 7, 8, 9, 10, 12, 23, 24, 25, 29, 52, 59]
-#myarr2-sorted = [3, 5, 6, 12, 32, 69]
-
-print(median_of_medians(myarr, 0, len(myarr), 1))
-print(median_of_medians(myarr2, 0, len(myarr2), 2))
-print(median_of_medians(myarr3, 0, len(myarr3), 5))
+a = input_array()
+k = int(input())
+print(median_of_medians(a, 0, len(a), k))
