22b5f351d5fe87b9427908ad9385cd1b638e8396
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-05-13 23:12:00 +0200

    add subclass for auxiliary min heap for tuples

--- 02_heap_select.py
+++ 02_heap_select.py
@@ -5,6 +5,7 @@ def input_array():
 class MinHeap:
     heap = []
 
+
     def left(self, i):
         j = 2*i + 1
         if j >= len(self.heap):
@@ -41,26 +42,46 @@ class MinHeap:
         self.heap = self.heap[:-1]
         self.heapify(0)
         
-    # uguale
-    def insert(self, values:tuple):
-        self.heap.append(values)
+
+    def insert(self, value):
+        self.heap.append(value)
         self.moveup(len(self.heap)-1)
         
-
+    
     def printHeap(self):
         for i in range(len(self.heap)):
             print(self.heap[i], end=" ")
         print('\n')
 
-
+    
     def buildheap(self, array):
         self.heap = array.copy()
         for i in range(len(self.heap) // 2, -1, -1):
             self.heapify(i)
         
+    
+    def heapify(self, i):
+        l = self.left(i)
+        r = self.right(i)
+        min = i
+        if l != None and self.heap[l] < self.heap[min]:
+            min = l
+        if r != None and self.heap[r] < self.heap[min]:
+            min = r
+        if min != i:
+            self.heap[i], self.heap[min] = self.heap[min], self.heap[i]
+            self.heapify(min)
 
-# assume che left(i) e right(i) siano sotto-heap valide
-# modifica la heap per creare anche i come sotto-heap valida 
+
+    def moveup(self, i):
+        p = self.parent(i)
+        if p != None and self.heap[i] < self.heap[p]:
+            self.heap[i], self.heap[p] = self.heap[p], self.heap[i]
+            self.moveup(p)
+
+
+# min heap che gestisce elementi formati da coppie (valore, indice)
+class AuxMinHeap(MinHeap):
     def heapify(self, i):
         l = self.left(i)
         r = self.right(i)
@@ -72,9 +93,8 @@ class MinHeap:
         if min != i:
             self.heap[i], self.heap[min] = self.heap[min], self.heap[i]
             self.heapify(min)
+    
 
-
-# muove i verso l'alto, fino a che l'ordine è corretto
     def moveup(self, i):
         p = self.parent(i)
         if p != None and self.heap[i][0] < self.heap[p][0]:
@@ -82,56 +102,28 @@ class MinHeap:
             self.moveup(p)
 
 
-myMinHeap = MinHeap()
-
 def heap_select(arr, k): # complessità O(n + k log k)
-    # main_heap o heap principale, creata con buildheap
     main_heap = MinHeap()
-    main_heap.buildheap(arr) # tempo O(n)
+    main_heap.buildheap(arr)
     
-    # aux_heap o heap ausiliaria, di coppie di interi, valore intero e posizione nella heap
-    aux_heap = MinHeap()
-    aux_heap.insert((main_heap.heap[0][0], 0))
+    aux_heap = AuxMinHeap()
+    aux_heap.insert((main_heap.heap[0], 0))
 
     for i in range(0, k-1): # k volte
         (val, ind) = aux_heap.getmin()
         aux_heap.extract()
 
-        # figli destro e sx che provengono dalla heap principale
+
         l = main_heap.left(ind)
         r = main_heap.right(ind)
         if l != None:
-            aux_heap.insert( (main_heap.heap[l][0], l) )
+            aux_heap.insert( (main_heap.heap[l], l) )
         if r != None:
-            aux_heap.insert( (main_heap.heap[r][0], r) )
+            aux_heap.insert( (main_heap.heap[r], r) )
     (val, ind) = aux_heap.getmin()        
     return val
 
 
-'''
-myMinHeap.insert((3, 0))
-myMinHeap.insert((4, 0))
-myMinHeap.insert((1, 0))
-myMinHeap.printHeap()
-print(myMinHeap.left(0))
-print(myMinHeap.right(0))
-print(myMinHeap.parent(1))
-print(myMinHeap.getmin())
-myMinHeap.extract()
-myMinHeap.printHeap()
-'''
-
-def tuplefy(arr):
-    tuples = []
-    for i in range(len(arr)):
-        tuples.append((arr[i], 0))
-    return tuples
-'''
-arr = [11, 6, 0, 9, 3, 5, 7]
-tuples = tuplefy(arr)
-'''
-
 inputArr = input_array()
 k = int(input())
-tuples = tuplefy(inputArr)
-print(heap_select(tuples, k))
+print(heap_select(inputArr, k))
