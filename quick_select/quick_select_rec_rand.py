231c83e511b43fb76e003505f53770fbd873d41c
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-05-23 16:14:43 +0200

    rename and relocate files

--- quick_select/quick_select_rec_rand.py
+++ quick_select/quick_select_rec_rand.py
@@ -1,12 +1,18 @@
 import random
 
-# QUICK SELECT RECURSIVE VERSION
+# QUICK SELECT RECURSIVE VERSION (RANDOMIZED PIVOT)
 
-def quick_select_rec(a, low, high, k):
+
+def quick_select_rec_rand(a, low, high, k):
     if not(are_parameters_valid(low, high, k)):
         return f"Index of k({k}) is out of range.\nPlease insert a valid index."
     else:
         return quick_select_tested(a, low, high, k)
+    
+
+# verify that the array is not empty and that the value of k is in array range  
+def are_parameters_valid(low, high, k):
+    return not ((high - low < 1) or (k-1 < low or k-1 > high-1))
 
 
 def quick_select_tested(a, low, high, k):                     
@@ -19,11 +25,6 @@ def quick_select_tested(a, low, high, k):
         return quick_select_tested(a, low, r, k)
     else: 
         return quick_select_tested(a, r+1, high, k)
-
-
-# verify that the array is not empty and that the value of k is in array range  
-def are_parameters_valid(low, high, k):
-    return not ((high - low < 1) or (k-1 < low or k-1 > high-1))
 
 
 def partition(a, low, high):
@@ -43,7 +44,7 @@ def rand_partition(a, low, high):
     return partition(a, low, high)
 
 
-'''Tests:
+#'''Tests:
 '''
 arr = [5, 4, 3, 2, 1]
 print(quick_select_rec(arr, 0, len(arr), 2))
@@ -51,3 +52,13 @@ print(quick_select_rec(arr, 0, len(arr), 4))
 print(quick_select_rec(arr, 0, len(arr), 6))
 '''
 #'''
+
+
+def input_array():
+    return [int(x) for x in input().split(" ") if x]
+
+'''
+a = input_array()
+k = int(input())
+print(quick_select_rec_rand(a, 0, len(a), k))
+'''
