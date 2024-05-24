e4b596a2c5ad48bbd72dc0161ab770b336553314
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-05-24 14:59:08 +0200

    add debugging prints to help debugging

--- quick_select/partition.py
+++ quick_select/partition.py
@@ -1,10 +1,22 @@
+# return the index of the last element of the array
+# if the same array was to be sorted
 def partition(arr, low, high):
-    pivot = arr[high-1]  
-    i = low - 1        
+    pivot = arr[high-1]
+    #print(f"pivot : {pivot}, arr[{high-1}]")
+    i = low - 1
+
+    iterations=0
 
     for j in range(low, high-1):
+        iterations += 1
         if arr[j] <= pivot:
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
+    #print(f"Total iterations: {iterations}")
     arr[i+1], arr[high-1] = arr[high-1], arr[i+1]
     return i+1
+
+'''
+arr = [0, 9, 1, 8, 2, 7, 3, 6, 4, 5]
+print(partition(arr, 0, len(arr)))
+'''
--- quick_select/rand_partition.py
+++ quick_select/rand_partition.py
@@ -4,5 +4,6 @@ import random
 
 def rand_partition(a, low, high):
     i = random.randint(low, high-1)
+    #print(f"random index i: {i}\nValue of a[i]: {a[i]}")
     a[high-1], a[i] = a[i], a[high-1]
     return partition.partition(a, low, high)
