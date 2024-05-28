75593a8334c3546c7e735e8fa6d89e6e7aa324bd
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-05-28 02:53:39 +0200

    update benchmark with imports of algorithm modules

--- benchmark.py
+++ benchmark.py
@@ -1,24 +1,16 @@
 import time
 import random
-import math 
 import matplotlib.pyplot as plt
-import algorithms
+import quick_select.quick_select_rec_rand as quick
+import heap_select.heap_select_min_max as heap
+import median_of_medians_select.med_of_meds_03 as median
 
 
-'''
-Ogni test su una lunghezza n data deve generare in modo pseudo-casuale 
-un array di n interi, in un intervallo a scelta (es. interi compresi fra 0 e 1000000) 
-e un parametro k∈{1,...,n}. Si noti che la complessità di alcuni algoritmi può dipendere 
-in modo significativo dalla scelta del range di interi e dal parametro k
-ed è quindi possibile immaginare test diversificati in funzione di queste variabili 
-allo scopo di evidenziare tali dipendenze.
-'''
-
-def init_array(n, max_val):
+def init_array(n, max_rand_val):
     random.seed()
     arr = [0] * n
     for i in range(n):
-        arr[i] = random.randint(0, max_val)
+        arr[i] = random.randint(0, max_rand_val)
     return arr
 
 
@@ -28,11 +20,6 @@ def init_indexes(n, tot_indexes):
         indexes[i] = random.randint(1, n-1)
     return indexes
 
-
-'''
-Il primo passo consiste nello stimare la risoluzione del clock di sistema,
-utilizzando un ciclo while per calcolare l'intervallo minimo di tempo misurabile. 
-'''
 
 def resolution():
     start = time.perf_counter()
@@ -52,48 +39,19 @@ def calculate_mean_resolution():
     return tot_sum / attempts
 
 
-#r = calculate_mean_resolution()
-
-'''
-In funzione della risoluzione stimata r e 
-dell'errore relativo massimo ammissibile (err = 0.001), 
-si calcola il tempo minimo misurabile
-
-Tmin = r*(1/err + 1)
-'''
-
-#err_min = 0.001
-
-#min_time = r * ((1/err_min) + 1)
-
-
-'''
-Per stimare il tempo medio di esecuzione di un algoritmo 
-su istanze dell'input di dimensione n si utilizza un ciclo while, 
-iterando l'esecuzione dell'algoritmo su un input di dimensione n, 
-generato in modo pseudo-casuale, e misurando complessivamente 
-un intervallo di tempo superiore a Tmin. 
-La misurazione deve essere effettuata senza interrompere il clock, 
-ovvero calcolando l'intero intervallo di tempo trascorso dall'inizio 
-dell'iterazione fino al momento il cui il tempo misurato risulti superiore a Tmin.
-Il tempo medio di esecuzione per una singola istanza di input sarà quindi ottenuto calcolando 
-il rapporto fra il tempo totale misurato e il numero di iterazioni dell'algoritmo eseguite 
-(questa divisione non influisce sull'errore relativo commesso).
-'''
-
-def measure(n, max_val, function, mean_resolution, tot_indexes):
+def measure(n, max_rand_val, function, mean_resolution, tot_indexes):
     min_err = 0.001
     min_time = mean_resolution * ((1/min_err) + 1)
     # n is the desired size of the array
     # min_time is the minumum measurable time to guarantee bounded relative error
     count = 0
-    # start_time = time.perf_counter()      <-----------  TODO (initially timer started here)
-    arr = init_array(n, max_val)
+    start_time = time.perf_counter()      #  <-----------  TODO (initially timer started here -see below-)
+    arr = init_array(n, max_rand_val)
     k_values = init_indexes(n, tot_indexes)
 
     while True:
-        # what if I started the timer here?  <----------  TODO
-        start_time = time.perf_counter()
+        # what if I started the timer here?  <----------  TODO ()
+        # start_time = time.perf_counter()
         
         for k in k_values:
             a_copy = arr.copy()
@@ -113,14 +71,10 @@ def main():
     n_min = 100
     n_max = 100000
     times = 100
-    tot_k_indices = 10
-    # if we want n = 100 for i = 0 then A = 100
+    tot_k_indices = 5
     A = n_min
-    # if A = 100, then 100*(B**i) = 100000 for i = 99,
-    # solving for B, we extract the 99th root of 1000 
-    # for finding B's value
     B = (n_max / n_min) ** (1/99)
-    points = [(None, None, None, None, None)] * times
+    points = [(None, None, None)] * times
 
     for i in range(times):
         print(f"\r{i} -> ", end='')
@@ -128,18 +82,20 @@ def main():
         print(f"valore di n: {n}")
         
         points[i] = (n, 
-            measure(n, 100, algorithms.quick_select, resolution, tot_k_indices),
-            measure(n, 100, algorithms.heap_select, resolution, tot_k_indices),
-            measure(n, 100, algorithms.median_of_medians, resolution, tot_k_indices),
+            measure(n, 1000000, quick.quick_select_rec_rand, resolution, tot_k_indices),
+            measure(n, 1000000, heap.heap_select, resolution, tot_k_indices),
+            measure(n, 1000000, median.median_of_medians_select, resolution, tot_k_indices),
+            #measure(n, 1000000, rec_rand.quick_select_rec_rand, resolution, tot_k_indices)
         )
 
-    xs, ys1, ys2, ys3, ys4 = zip(*points)
+    xs, ys1, ys2, ys3 = zip(*points)
     plt.xscale('log')
     plt.yscale('log')
-    plt.scatter(xs, ys1, c='green') # quick_select
-    plt.scatter(xs, ys2, c='blue') # heap_select
-    plt.scatter(xs, ys3, c='red') # median_of_medians
-    plt.scatter(xs, ys4, c='orange')
+    plt.scatter(xs, ys1, c='blue', label='Quick Select')  
+    plt.scatter(xs, ys2, c='green', label='Heap Select')
+    plt.scatter(xs, ys3, c='orange', label='Median of Medians Select')   
+    #plt.scatter(xs, ys4, c='orange', label="Recursive Random Pivot")    # recursion random
+    plt.legend(title="Median of Medians Select")
     plt.show()
 
 main()
