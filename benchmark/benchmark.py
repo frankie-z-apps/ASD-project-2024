0721e7f073958e526202d53457dc18170108515c
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-06-27 13:54:25 +0200

    polish code and add explanatory comments

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -7,46 +7,41 @@ import heap_select.heap_select_min_max as heap
 import median_of_medians_select.med_of_meds_03 as medians
 
 
-def init_array(n, max_rand_val):
-    random.seed()
+def init_array(n, max_rand_val):                                        # initialize the array with random values
+    random.seed()                                                       # in the interval [0, max_rand_val]
     arr = [0] * n
     for i in range(n):
         arr[i] = random.randint(0, max_rand_val)
     return arr
 
 
-def init_indexes(n, tot_indexes):
-    indexes = [0] * tot_indexes
-    for i in range(tot_indexes):
+def init_indexes(n, k):                                                 # extract k random indexes in the interval [0, n-1] 
+    indexes = [0] * k                                                   # from an array of length n
+    for i in range(k):
         indexes[i] = random.randint(1, n-1)
     return indexes
 
 
-def resolution():
-    start = time.monotonic()
+def resolution():                                                       # calculate the smallest time interval  
+    start = time.monotonic()                                            # recorded from the system
     while time.monotonic() == start:
         pass
     stop = time.monotonic()
     return stop - start
 
 
-# result of resoluion() function can vary in time,
-# so we calculate a mean value
-def calculate_mean_resolution():
-    attempts = 1000
+def calculate_mean_resolution(n_attempts):                              # calculate the mean resolution over n total attempts
     tot_sum = 0    
-    for i in range(attempts):
+    for i in range(n_attempts):
         tot_sum += resolution()
-    return tot_sum / attempts
+    return tot_sum / n_attempts
 
 
-def measure(arr, function, mean_resolution, k_list):
-    min_err = 0.001
-    min_time = mean_resolution * ((1/min_err) + 1)
-    # n is the desired size of the array
-    # min_time is the minumum measurable time to guarantee bounded relative error
-    count = 0
-    start_time = time.monotonic()      #  <-----------  TODO (initially timer started here -see below-)
+def measure(arr, function, mean_resolution, k_list):                    # measure the mean time required for the execution 
+    min_err = 0.001                                                     # of an algorithm over an array, over a list of many
+    min_time = mean_resolution * ((1/min_err) + 1)                      # values of k. min_time is the minimum measurable time
+    count = 0                                                           # to guarantee bounded relative error
+    start_time = time.monotonic()
 
     while True:        
         for k in k_list:
@@ -61,29 +56,28 @@ def measure(arr, function, mean_resolution, k_list):
     return (end_time - start_time) / count
 
 
+
 def main():
-    resolution = calculate_mean_resolution()
+    resolution = calculate_mean_resolution(1000)
     max_rand_val = 1000000
     n_begin = 100
-    n_end = 100000
+    n_end = 10000
     times = 100
-    k_tests = 5
+    k_tests = 100
     A = n_begin
     B = (n_end / n_begin) ** (1/99)
     points = [(None, None, None)] * times
 
     main_duration_start = time.monotonic()
 
-
     for i in range(times):
         print(f"\r{i} -> ", end='')
         n = int(A * (B ** i))
-        print(f"valore di n: {n}")
+        print(f"Value of n: {n}")
 
         arr = init_array(n, max_rand_val)
         k_values = init_indexes(n, k_tests)
 
-        
         points[i] = (n, 
             measure(arr, quick.quick_select_rec_rand, resolution, k_values),
             measure(arr, heap.heap_select, resolution, k_values),
@@ -100,8 +94,20 @@ def main():
     plt.scatter(xs, ys3, c='orange', label='Median of Medians Select')   
     plt.legend(title="Algorithms comparison")
 
-    plt.annotate(f'Number of k-tests for array: {k_tests}\nMax random value for array element: {max_rand_val}\nTotal execution time (seconds): {main_duration_end - main_duration_start}',xy=(0.0, -0.12), xycoords='axes fraction', ha='left', fontsize=7)
-    plt.annotate(f'Minimum array length: {n_begin}\nMaximum array length: {n_end}', xy=(1.0, -0.12), xycoords='axes fraction', ha='right', fontsize=7 )
+    plt.annotate(f'Number of k-tests for array: {k_tests}\n\
+            Max random value for array element: {max_rand_val}\n\
+            Total execution time (seconds): {main_duration_end - main_duration_start}', \
+                xy=(0.0, -0.12), \
+                xycoords='axes fraction', \
+                ha='left', \
+                fontsize=7)
+    
+    plt.annotate(f'Minimum array length: {n_begin}\n\
+                Maximum array length: {n_end}', \
+                xy=(1.0, -0.12), \
+                xycoords='axes fraction', \
+                ha='right', \
+                fontsize=7 )
 
     plt.show()
 
