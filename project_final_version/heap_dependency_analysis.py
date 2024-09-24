14b827e3508caf06f37cb3a1875b1158dd729662
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-09-25 01:11:37 +0200

    refactor and add comments

--- project_final_version/heap_dependency_analysis.py
+++ project_final_version/heap_dependency_analysis.py
@@ -4,41 +4,52 @@ import matplotlib.pyplot as plt
 
 import heap_select as heap
 
-# DEPENDENCY ANALYSIS: how k parameter affects performance of heap-select algorithm
+# DEPENDENCY ANALYSIS: 
+# how k parameter affects performance of heap-select algorithm
 
-
-def init_array(n, max_rand_val):                                        # initialize the array with random values
-    random.seed()                                                       # in the interval [0, max_rand_val]
+'''
+    Initialize array with random values
+    in interval [0, max_rand_val]
+'''
+def init_array(n, max_rand_val):                                        
+    random.seed()                                                       
     arr = [0] * n
     for i in range(n):
         arr[i] = random.randint(0, max_rand_val)
     return arr
 
 
-def init_indexes(n):                                                    # return sorted list of indexes [0, 1, ... n-1]
-    indexes = [i for i in range(n)]
-    return indexes
-
-
-def resolution():                                                       # calculate the smallest time interval  
-    start = time.monotonic()                                            # recorded from the system
+'''
+    Calculate smallest time interval
+    recorded by system
+'''
+def resolution():                                                       
+    start = time.monotonic()                                            
     while time.monotonic() == start:
         pass
     stop = time.monotonic()
     return stop - start
 
 
-def calculate_mean_resolution(n_attempts):                              # calculate the mean resolution over n total attempts
+'''
+    Calculate mean resolution
+    over n total iterations
+'''
+def calculate_mean_resolution(iterations):                              
     tot_sum = 0    
-    for i in range(n_attempts):
+    for i in range(iterations):
         tot_sum += resolution()
-    return tot_sum / n_attempts
+    return tot_sum / iterations
 
 
-def measure(arr, function, mean_resolution, k):                         # measure the mean time required for the execution 
-    min_err = 0.001                                                     # of an algorithm over an array, over a list of many
-    min_time = mean_resolution * ((1/min_err) + 1)                      # values of k. min_time is the minimum measurable time
-    count = 0                                                           # to guarantee bounded relative error
+'''
+   Measure the time required for a single execution
+   of function on array with given k
+'''
+def measure(arr, function, mean_resolution, k):                         
+    min_err = 0.001                                                     
+    min_time = mean_resolution * ((1/min_err) + 1)                      
+    count = 0                                                           
     start_time = time.monotonic()
 
     while True:        
@@ -53,44 +64,42 @@ def measure(arr, function, mean_resolution, k):                         # measur
     return (end_time - start_time) / count
 
 
-
+'''
+    Test heap select algorithm on given array size
+    k parameter iterates over the entire array
+    so that each and every index is passed only once
+    in the function call
+    Measure time required for completion
+    Plot results and show graph
+'''
 def main():
     array_length = int(input("Input test array size: "))
     resolution = calculate_mean_resolution(1000)
     max_rand_val = 1000000
-    #n_begin = 100
-    #n_end = 10000
-    #k_tests = 100
-    #A = n_begin
-    #B = (n_end / n_begin) ** (1/99)
-    points = [(None, None)] * array_length
-
-    arr = init_array(array_length, 1000000)
-    k_values = init_indexes(array_length)
+    points = [(None)] * array_length
+    arr = init_array(array_length, max_rand_val)
 
     main_duration_start = time.monotonic()
 
     for i in range(array_length):
-        #print(f"\r{i} -> ", end='')
-        #n = int(A * (B ** i))
-        print(f"Test number: {i+1} - {int((i / array_length)*100)}%", end='\r')
+        print(f"Test number: {i+1}  Progress: {int((i / array_length)*100)}%", end='\r')
 
         points[i] = (i, 
             measure(arr, heap.heap_select, resolution, i),
-            measure(arr, heap.min_heap_select, resolution, i)
+            #measure(arr, heap.min_heap_select, resolution, i)
         )
 
     main_duration_end = time.monotonic()
 
-    xs, ys1, ys2 = zip(*points)
+    xs, ys1 = zip(*points)
     plt.xscale('linear')
     plt.yscale('linear')
-    plt.scatter(xs, ys1, c='darkblue', label='Min-Max heap')  
-    plt.scatter(xs, ys2, c='lightblue', label='Min heap')
-    plt.legend(title="K-dependence analysis")
+    plt.scatter(xs, ys1, c='lightgreen', label='Min-Max heap')  
+    #plt.scatter(xs, ys2, c='lightblue', label='Min heap')
+    plt.legend(title="K-dependency analysis")
 
-    '''
-    plt.annotate(f'Number of k-tests for array: {k_tests}\n\
+
+    plt.annotate(f'Array size: {array_length}\n\
             Max random value for array element: {max_rand_val}\n\
             Total execution time (seconds): {main_duration_end - main_duration_start}', \
                 xy=(0.0, -0.12), \
@@ -98,14 +107,7 @@ def main():
                 ha='left', \
                 fontsize=7)
     
-    plt.annotate(f'Minimum array length: {n_begin}\n\
-                Maximum array length: {n_end}', \
-                xy=(1.0, -0.12), \
-                xycoords='axes fraction', \
-                ha='right', \
-                fontsize=7 )
-    
-    
+    '''
     # Disegna le rette di riferimento
 
     # Riferimento lineare per QuickSelect
