24e2925ccfd86e74dd095a1ed8454c8070c63bb6
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-09-24 18:41:37 +0200

    refactor benchmark

--- project_final_version/benchmark.py
+++ project_final_version/benchmark.py
@@ -2,45 +2,65 @@ import time
 import random
 import matplotlib.pyplot as plt
 
-import quick_select.quick_select_rec_rand as quick
-import heap_select.heap_select_min_max as heap
-import median_of_medians_select.med_of_meds_03 as medians
+import quick_select as quick
+import heap_select as heap
+import median_of_medians_select as median
 
-
-def init_array(n, max_rand_val):                                        # initialize the array with random values
-    random.seed()                                                       # in the interval [0, max_rand_val]
+'''
+    initialize array with random values
+    in interval [0, max_rand_val]
+'''
+def init_array(n, max_rand_val):                                        
+    random.seed()                                                       
     arr = [0] * n
     for i in range(n):
         arr[i] = random.randint(0, max_rand_val)
     return arr
 
 
-def init_indexes(n, k):                                                 # extract k random indexes in the interval [0, n-1] 
-    indexes = [0] * k                                                   # from an array of length n
+'''
+    extract k random indexes
+    in interval [0, n-1]
+'''
+def init_indexes(n, k):                                                 
+    indexes = [0] * k                                                   
     for i in range(k):
         indexes[i] = random.randint(1, n-1)
     return indexes
 
 
-def resolution():                                                       # calculate the smallest time interval  
-    start = time.monotonic()                                            # recorded from the system
+'''
+    calculate smallest time interval
+    recorded by system
+'''
+def resolution():                                                      
+    start = time.monotonic()                                           
     while time.monotonic() == start:
         pass
     stop = time.monotonic()
     return stop - start
 
 
-def calculate_mean_resolution(n_attempts):                              # calculate the mean resolution over n total attempts
+'''
+    calculate mean resolution
+    over n total times
+'''
+def calculate_mean_resolution(times):                              
     tot_sum = 0    
-    for i in range(n_attempts):
+    for i in range(times):
         tot_sum += resolution()
-    return tot_sum / n_attempts
+    return tot_sum / times
 
 
-def measure(arr, function, mean_resolution, k_list):                    # measure the mean time required for the execution 
-    min_err = 0.001                                                     # of an algorithm over an array, over a list of many
-    min_time = mean_resolution * ((1/min_err) + 1)                      # values of k. min_time is the minimum measurable time
-    count = 0                                                           # to guarantee bounded relative error
+
+'''
+    measure the mean time required for the execution
+    of a function with multiple k-values on the same array
+'''
+def measure(arr, function, mean_resolution, k_list):                    
+    min_err = 0.001                                                     
+    min_time = mean_resolution * ((1/min_err) + 1)                      
+    count = 0                                                           
     start_time = time.monotonic()
 
     while True:        
@@ -60,7 +80,7 @@ def measure(arr, function, mean_resolution, k_list):                    # measur
 def main():
     resolution = calculate_mean_resolution(1000)
     max_rand_val = 1000000
-    n_begin = 100
+    n_begin = 10
     n_end = 100000
     times = 100
     k_tests = 10
@@ -71,17 +91,16 @@ def main():
     main_duration_start = time.monotonic()
 
     for i in range(times):
-        print(f"\r{i} -> ", end='')
         n = int(A * (B ** i))
-        print(f"Value of n: {n}")
+        print(f"Progress: {i}%\t Array size: {n}", end='\r')
 
         arr = init_array(n, max_rand_val)
         k_values = init_indexes(n, k_tests)
 
         points[i] = (n, 
-            measure(arr, quick.quick_select_rec_rand, resolution, k_values),
+            measure(arr, quick.quick_select, resolution, k_values),
             measure(arr, heap.heap_select, resolution, k_values),
-            measure(arr, medians.median_of_medians_select, resolution, k_values),
+            measure(arr, median.median_of_medians_select, resolution, k_values),
         )
 
     main_duration_end = time.monotonic()
@@ -89,8 +108,8 @@ def main():
     xs, ys1, ys2, ys3 = zip(*points)
     plt.xscale('log')
     plt.yscale('log')
-    plt.scatter(xs, ys1, c='blue', label='Quick Select')  
-    plt.scatter(xs, ys2, c='green', label='Heap Select')
+    plt.scatter(xs, ys1, c='lightblue', label='Quick Select')  
+    plt.scatter(xs, ys2, c='lightgreen', label='Heap Select')
     plt.scatter(xs, ys3, c='orange', label='Median of Medians Select')   
     plt.legend(title="Algorithms comparison")
 
@@ -115,12 +134,12 @@ def main():
     # Riferimento lineare per QuickSelect
     k_qs = ys1[0] / xs[0]  # Calcola il coefficiente di proporzionalità
     tempoLineare_qs = [(k_qs * x) for x in xs]  # Rette lineare
-    plt.plot(xs, tempoLineare_qs, '--', color='green', label='Andamento Lineare QS')
+    plt.plot(xs, tempoLineare_qs, '--', color='blue', label='Andamento Lineare QS')
 
     # Riferimento lineare per HeapSelect
     k_hs = ys2[0] / xs[0]  # Calcola il coefficiente di proporzionalità
     tempoLineare_hs = [(k_hs * x) for x in xs]  # Rette lineare
-    plt.plot(xs, tempoLineare_hs, '--', color='blue', label='Andamento Lineare HS')
+    plt.plot(xs, tempoLineare_hs, '--', color='green', label='Andamento Lineare HS')
 
     
     # Riferimento lineare per MedianOfMedians
