165493da3f4152eb82af3e9cb6e3af242cc3b5ff
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-10-05 19:00:28 +0200

    update last version of heap select

--- project_final_version/heap_select.py
+++ project_final_version/heap_select.py
@@ -2,42 +2,52 @@
 
 class MaxHeap:
     def __init__(self):
-        # We use a list to represent the heap
         self.heap = []
 
 
-    # Helper function to get the parent index
+    '''
+        Get parent index
+    '''
     def parent(self, index):
         return (index - 1) // 2
 
 
-    # Helper function to get the left child index
+    '''
+        Get left child index
+    '''
     def left(self, index):
         return 2 * index + 1
 
 
-    # Helper function to get the right child index
+    '''
+        Get right child index
+    '''
     def right(self, index):
         return 2 * index + 2
 
 
-    # Swap helper function to keep the code clean
+    '''
+        Swap elements of index i and j
+    '''
     def swap(self, i, j):
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
 
-    # Insert an element into the heap
+    '''
+        Insert new element into MaxHeap
+    '''
     def insert(self, key):
-        # Add the new element to the end of the list
         self.heap.append(key)
-        # Bubble it up to maintain the heap property
-        self.move_up(len(self.heap) - 1)
+        self.move_up(self.size() - 1)
 
 
+    '''
+        Move up the element at index until heap property is restored
+    '''
     def move_up(self, index):
-        # Bubble up the element at index until the heap property is restored
         while index > 0:
             parent = self.parent(index)
+
             if self.heap[index] > self.heap[parent]:
                 self.swap(index, parent)
                 index = parent
@@ -45,32 +55,33 @@ class MaxHeap:
                 break
 
 
-    # Extract the maximum element (root)
+    '''
+        Extract max element (root)
+    '''
     def extract_max(self):
-        if len(self.heap) == 0:
+        if self.is_empty():
             raise IndexError("extract_max(): empty heap")
-        # Replace the root of the heap with the last element
-        self.swap(0, len(self.heap) - 1)
-        max_value = self.heap.pop()  # Remove the last element (the original root)
-        # Bubble down the new root to restore heap property
-        self.move_down(0)
+        self.swap(0, self.size() - 1)
+        max_value = self.heap.pop()
+        self.heapify(0)
         return max_value
 
+    '''
+        Move down element at index until heap property is restored
+    '''
+    def heapify(self, index):
+        size = self.size()
 
-    def move_down(self, index):
-        size = len(self.heap)
         while index < size:
             left = self.left(index)
             right = self.right(index)
             largest = index
 
-            # Find the largest of the current index and its children
             if left < size and self.heap[left] > self.heap[largest]:
                 largest = left
             if right < size and self.heap[right] > self.heap[largest]:
                 largest = right
 
-            # If the largest is not the current index, swap and continue bubbling down
             if largest != index:
                 self.swap(index, largest)
                 index = largest
@@ -78,70 +89,87 @@ class MaxHeap:
                 break
 
 
-    # Peek at the maximum element without removing it
+    '''
+        Peek at max element (root) without removing it
+    '''
     def peek_max(self):
-        if len(self.heap) == 0:
+        if self.is_empty():
             raise IndexError("peek_max(): empty heap")
         return self.heap[0]
 
 
-    # Heapify an arbitrary list (in-place conversion)
-    def heapify(self, arr):
+    '''
+        Build a MaxHeap starting from a generic array
+    '''
+    def build_heap(self, arr):
         self.heap = arr
-        # Start from the first non-leaf node and bubble down
-        for i in range((len(self.heap) // 2) - 1, -1, -1):
-            self.move_down(i)
+        for i in range((self.size() // 2) - 1, -1, -1):
+            self.heapify(i)
 
 
-    # Get the size of the heap
+    '''
+        Get heap size
+    '''
     def size(self):
         return len(self.heap)
 
 
-    # Check if the heap is empty
+    '''
+        Check if heap is empty
+    '''
     def is_empty(self):
-        return len(self.heap) == 0
+        return self.size() == 0
 
    
 
 class MinHeap:
     def __init__(self):
-        # We use a list to represent the heap
         self.heap = []
 
 
-    # Helper function to get the parent index
+    '''
+        Get parent index
+    '''
     def parent(self, index):
         return (index - 1) // 2
 
 
-    # Helper function to get the left child index
+    '''
+        Get left child index
+    '''
     def left(self, index):
         return 2 * index + 1
 
 
-    # Helper function to get the right child index
+    '''
+        Get right child index
+    '''
     def right(self, index):
         return 2 * index + 2
 
 
-    # Swap helper function to keep the code clean
+    '''
+        Swap elements of index i and j
+    '''
     def swap(self, i, j):
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
 
-    # Insert an element into the heap
+    '''
+        Insert new element into MaxHeap
+    '''
     def insert(self, key):
-        # Add the new element to the end of the list
         self.heap.append(key)
-        # Bubble it up to maintain the heap property
-        self.move_up(len(self.heap) - 1)
+        self.move_up(self.size() - 1)
 
 
+    '''
+        Move up the element at index until heap property is restored
+    '''
     def move_up(self, index):
-        # Bubble up the element at index until the heap property is restored
         while index > 0:
             parent = self.parent(index)
+
             if self.heap[index] < self.heap[parent]:
                 self.swap(index, parent)
                 index = parent
@@ -149,32 +177,34 @@ class MinHeap:
                 break
 
 
-    # Extract the maximum element (root)
+    '''
+        Extract min element (root)
+    '''
     def extract_min(self):
-        if len(self.heap) == 0:
+        if self.is_empty():
             raise IndexError("extract_min(): empty heap")
-        # Replace the root of the heap with the last element
-        self.swap(0, len(self.heap) - 1)
-        min_value = self.heap.pop()  # Remove the last element (the original root)
-        # Bubble down the new root to restore heap property
-        self.move_down(0)
+        self.swap(0, self.size() - 1)
+        min_value = self.heap.pop()
+        self.heapify(0)
         return min_value
 
 
-    def move_down(self, index):
-        size = len(self.heap)
+    '''
+        Move down element at index until heap property is restored
+    '''
+    def heapify(self, index):
+        size = self.size()
+
         while index < size:
             left = self.left(index)
             right = self.right(index)
             smallest = index
 
-            # Find the smallest of the current index and its children
             if left < size and self.heap[left] < self.heap[smallest]:
                 smallest = left
             if right < size and self.heap[right] < self.heap[smallest]:
                 smallest = right
 
-            # If the smallest is not the current index, swap and continue bubbling down
             if smallest != index:
                 self.swap(index, smallest)
                 index = smallest
@@ -182,68 +212,36 @@ class MinHeap:
                 break
 
 
-    # Peek at the minimum element without removing it
+    '''
+        Peek at min element (root) without removing it
+    '''
     def peek_min(self):
-        if len(self.heap) == 0:
+        if self.is_empty():
             raise IndexError("peek_min(): empty heap")
         return self.heap[0]
 
 
-    # Heapify an arbitrary list (in-place conversion)
-    def heapify(self, arr):
+    '''
+        Build a MinHeap starting from a generic array
+    '''
+    def build_heap(self, arr):
         self.heap = arr
-        # Start from the first non-leaf node and bubble down
-        for i in range((len(self.heap) // 2) - 1, -1, -1):
-            self.move_down(i)
+        for i in range((self.size() // 2) - 1, -1, -1):
+            self.heapify(i)
 
 
-    # Get the size of the heap
+    '''
+        Get heap size
+    '''
     def size(self):
         return len(self.heap)
 
 
-    # Check if the heap is empty
+    '''
+        Check if heap is empty
+    '''
     def is_empty(self):
-        return len(self.heap) == 0
-
-
-'''
-    Representation of an auxiliary Min-Heap in which elements are pairs of the form [value, index].
-    The heap position of each element is based on its 'value' field.
-'''
-class AuxMinHeap(MinHeap):
-    def aux_min_move_down(self, index):
-        size = len(self.heap)
-        while index < size:
-            left = self.left(index)
-            right = self.right(index)
-            smallest = index
-
-
-            # Find the smallest of the current index and its children
-            if left < size and self.heap[left][0] < self.heap[smallest][0]:
-                smallest = left
-            if right < size and self.heap[right][0] < self.heap[smallest][0]:
-                smallest = right
-
-
-            # If the largest is not the current index, swap and continue bubbling down
-            if smallest != index:
-                self.swap(index, smallest)
-                index = smallest
-            else:
-                break
-    
-
-    def aux_min_move_up(self, index):
-        # Bubble up the element at index until the heap property is restored
-        while index > 0:
-            parent = self.parent(index)
-            if self.heap[index][0] < self.heap[parent][0]:
-                self.swap(index, parent)
-                index = parent
-            else:
-                break
+        return self.size() == 0
 
 
 '''
@@ -251,22 +249,21 @@ class AuxMinHeap(MinHeap):
     The heap position of each element is based on its 'value' field.
 '''
 class AuxMaxHeap(MaxHeap):
-    def aux_max_move_down(self, index):
-        size = len(self.heap)
+    '''
+        Move down element at index until heap property is restored
+    '''
+    def aux_max_heapify(self, index):
+        size = self.size()
         while index < size:
             left = self.left(index)
             right = self.right(index)
             largest = index
-
-
-            # Find the largest of the current index and its children
+            
             if left < size and self.heap[left][0] > self.heap[largest][0]:
                 largest = left
             if right < size and self.heap[right][0] > self.heap[largest][0]:
                 largest = right
 
-
-            # If the largest is not the current index, swap and continue bubbling down
             if largest != index:
                 self.swap(index, largest)
                 index = largest
@@ -274,11 +271,56 @@ class AuxMaxHeap(MaxHeap):
                 break
     
 
+    '''
+        Move up element at index until heap property is restored
+    '''
     def aux_max_move_up(self, index):
-        # Bubble up the element at index until the heap property is restored
         while index > 0:
             parent = self.parent(index)
+
             if self.heap[index][0] > self.heap[parent][0]:
+                self.swap(index, parent)
+                index = parent
+            else:
+                break
+
+
+
+'''
+    Representation of an auxiliary Min-Heap in which elements are pairs of the form [value, index].
+    The heap position of each element is based on its 'value' field.
+'''
+class AuxMinHeap(MinHeap):
+    '''
+        Move down element at index until heap property is restored
+    '''
+    def aux_min_heapify(self, index):
+        size = self.size()
+        while index < size:
+            left = self.left(index)
+            right = self.right(index)
+            smallest = index
+
+            if left < size and self.heap[left][0] < self.heap[smallest][0]:
+                smallest = left
+            if right < size and self.heap[right][0] < self.heap[smallest][0]:
+                smallest = right
+            
+            if smallest != index:
+                self.swap(index, smallest)
+                index = smallest
+            else:
+                break
+    
+
+    '''
+        Move up element at index until heap property is restored
+    '''
+    def aux_min_move_up(self, index):
+        while index > 0:
+            parent = self.parent(index)
+     
+            if self.heap[index][0] < self.heap[parent][0]:
                 self.swap(index, parent)
                 index = parent
             else:
@@ -297,10 +339,13 @@ def heap_select(arr, start, end, k):
         return select_min_heap(arr, None, None, k)
 
 
+'''
+    Execute algorithm using a MaxHeap
+'''
 def select_max_heap(arr, start, end, k):
     main_heap = MaxHeap()
     aux_heap = AuxMaxHeap()
-    main_heap.heapify(arr)
+    main_heap.build_heap(arr)
     aux_heap.insert((main_heap.heap[0], 0))
 
     for i in range(len(arr) - k):
@@ -317,10 +362,13 @@ def select_max_heap(arr, start, end, k):
     return val
 
 
+'''
+    Execute algorithm using a MinHeap
+'''
 def select_min_heap(arr, start, end, k):
     main_heap = MinHeap()
     aux_heap = AuxMinHeap()
-    main_heap.heapify(arr)
+    main_heap.build_heap(arr)
     aux_heap.insert((main_heap.heap[0], 0))
 
     for i in range(k-1):
